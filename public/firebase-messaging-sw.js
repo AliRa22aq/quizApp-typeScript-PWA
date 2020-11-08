29822676e07d0c5ca08f7a2ab325712d1e724dd5
Ali Razzaq <59477206+AliRa22aq@users.noreply.github.com>
2020-11-08 23:26:11 +0500

    Initializing - Firebase

---
+++ public/firebase-messaging-sw.js
@@ -0,0 +1,17 @@
+importScripts('/__/firebase/8.0.0/firebase-app.js');
+importScripts('/__/firebase/8.0.0/firebase-messaging.js');
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDagQMBYEy5IExKRnGrJW2J-tgoAzxp858",
+    authDomain: "quiz-app-pwa-pushnotifications.firebaseapp.com",
+    databaseURL: "https://quiz-app-pwa-pushnotifications.firebaseio.com",
+    projectId: "quiz-app-pwa-pushnotifications",
+    storageBucket: "quiz-app-pwa-pushnotifications.appspot.com",
+    messagingSenderId: "78708729144",
+    appId: "1:78708729144:web:7feca09509c5ec92400dcd",
+    measurementId: "G-SVZ2B2M8PM"
+  };
+
+
+  firebase.initializeApp(firebaseConfig);
+  firebase.messaging();
